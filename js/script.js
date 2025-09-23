568a987c2c1d320b12d14fe7398fd1e37001cc0c
simon0302010 <simon0302010@gmail.com>
2025-09-23 11:46:43 +0200

    Added Hackatime integration to display time spent on project

--- js/script.js
+++ js/script.js
@@ -3,14 +3,39 @@ fetch('projects.json')
   .then(projects => {
     const container = document.querySelector('.projects-container');
     container.innerHTML = '';
-    projects.forEach(project => {
-      const div = document.createElement('div');
-      div.className = 'project';
-      div.innerHTML = `
-        <img src="${project.image}" alt="${project.title}" class="project-image">
-        <div class="project-title">${project.title}</div>
-        <div class="project-desc">${project.desc}</div>
-      `;
-      container.appendChild(div);
-    });
+    // TODO: add caching
+    fetch('https://hackatime.hackclub.com/api/v1/users/simon/projects/details')
+      .then(res => res.json())
+      .then(hackatimeData => {
+        projects.forEach(project => {
+          const div = document.createElement('div');
+          div.className = 'project';
+          div.innerHTML = `
+            <img src="${project.image}" alt="${project.title}" class="project-image">
+            <div class="project-title">${project.title}</div>
+            <div class="project-desc">${project.desc}</div>
+            <div class="project-time"><br>Loading...</div>
+          `;
+          container.appendChild(div);
+
+          // find the project
+          const match = hackatimeData.projects.find(
+            p => p.name.toLowerCase() === project.hackatime.toLowerCase()
+          );
+          if (match) {
+            const hours = Math.round(match.total_seconds / 3600);
+            div.querySelector('.project-time').innerHTML = `<br>Time spent: ${hours}h`;
+          } else {
+            div.querySelector('.project-time').innerHTML = '<br>No HackaTime data';
+          }
+        });
+      })
+      .catch(() => {
+        // show error on fail
+        projects.forEach(() => {
+          const div = document.createElement('div');
+          div.className = 'project';
+          div.querySelector('.project-time').textContent = 'Error loading HackaTime';
+        });
+      });
   });
